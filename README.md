faec649722ecf6ec9ac437609cc802acab297e8d
Crystal-Link <calvinzheng0119@gmail.com>
2022-05-08 05:54:04 -0400

    Added attacking mechanic

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# CPE551-Work
-Work for CPE/EE/AAI 551, Engineering Programming: Python
+# Python Battleship
+
+###### This is the final project for CPE/EE/AAI 551, Engineering Programming: Python.
+This is the Battleship game built using Python. A GUI is implemented through the use of the PyGame and Tkinter Packages.
+
+## Concept
+1. Create a GUI Battleship board/grid.
+2. Allow the player to select which cells they wish to place their ships in before the game starts
+	- I've gone with the standard rules where players place 5 ships of various sizes on a 10 by 10 grid
+	- The ships are of sizes 2, 3, 3, 4, and 5.
+3. Once the game starts, the player gets to take turns with the CPU firing missiles onto the opposing board, attempting to hit and destroy the opponent's ships.
+	- If a player lands a hit, they get to fire another missile.
+4. Game ends when all of a player's ships have been destroyed.
+5. Implement a multiplayer version that allows players to play against each other rather than against the CPU.
+
+## Steps
+### 1. Generating the GUI in PyGame
+The first step was to figure out how to use the PyGame package and its modules to generate a GUI and draw the battleship boards onto it. Since this was my first time using PyGame, I was not familiar with how it works and have to look into the package's documentations as well as look for some video tutorials showcasing it. 
+
+The main question that I had at the start of this project was "How am I going to implement the different aspects of Battleship?" I was not sure how I wanted the game to look and how I wanted the user to perform the actions required to play the game. After looking through several PyGame tutorials and featured games, I came across a version of [PyGame Battleship](https://github.com/igor-bond16/Battle_Ship_Game/tree/master/Battle%20Ship%20Complete) created by Japanese user [@Bond Igor](https://github.com/igor-bond16). He showcased his version of Battleship in a [YouTube video](https://www.youtube.com/watch?v=jZ3F4kNnxM4) and I really loved his idea of using PyGame to draw the two boards and display the current status of the game, then use a separate Tkinter window filled with buttons to perform actions. Therefore, I decided to adopt his implementation of Battleship and use his source code as an example for my game. After going through his source code, I noticed that it was not coded using Object-oriented programming (OOP) and his version of Battleship strayed from the standard rules and was missing several key features of the game. Therefore I am only going to refer to his game for the implementation of it using PyGame and Tkinter.
+
+To generate the GUI in PyGame, I had to first initialize PyGame, as well as the variables/settings that I am going to use for the PyGame window. Then, I created the `Board` class which includes the functions to draw the board and its axis titles within the PyGame window. At this point, the game looks like this:
+
+![initial_board.PNG](./Images/initial_board.PNG)
+
+### 2. Player Ship Generation (Letting the user place ships on the board)
+The next step was to work on generating ships onto the PyGame board. Remember that in Igor's version, the user was able to click buttons on an external Tkinter window to determine where the player's ships will be placed. Following his approach, I created a `Ships` class that initializes a Tkinter instance and have it generate a block of buttons, one for each cell on the board. The user will be able to click on the cell they want their ship to be on and the button will turn green. If they change their mind, they can click the button again to undo it. The idea is that these buttons will append the cell index into an array, which will determine the cells that get filled in on the game board. At this point, the game looks like this:
+
+![initial_player_ships.gif](./Images/initial_player_ships.gif)
+
+I have not yet constrained the cells that the player is able to select (since they should only be able to select the cells for their 5 ships), but I will implement that at a later step.
+
+### 3. Enemy Ship Generation (Single-Player)
+Currently, the game is single-player, therefore the enemy (in this case, CPU), must be able to generate their 5 ships randomly. Since I had already set up the method to draw the ships onto the board, it was just a matter of randomly generating the cell indexes that will be appended to the array to be drawn. To do this, a main loop will run until all ships are generated. First, I had to first randomize the the orientation of each ship, either horizontal or veritcal. Then, based on the orientation, a two integers will be randomly generated, which will represent the coordinate of the ship on the board. A loop will run based on the size of the ship to check if any of the cells of the ship collides with another ship. If it does, skip this ship. After the loop ends, all 5 ships of various sizes should be placed. To see this work, I had the game temporarily draw this on the enemy's board:
+
+![initial_enemy_ships.gif](./Images/initial_enemy_ships.gif)
+
+### 4. Attacking Mechanic
+The next step, now that both sides are able to generate their ships, is to allow both sides to attack each other. To allow the player to attack, a new Tkinter window will pop up that allows the player to select which cell it wants to attack. Once clicked, the button will be disabled and the button as well as the enemy's board will show the cell as either black or red, depending on if a ship was hit or not. If the player hits a ship, they get to go again. Otherwise, the enemy gets to take a shot and also gets to go again should they land a hit. The CPU will attack randomly until they hit a ship. At this point the game looks like this:
+
+![initial_attack.gif](./Images/initial_attack.gif)
+
+###### Note: The enemy ships are still displayed for testing purposes.
+
+Once the CPU hit a ship, it is only logical to check the cells around it and attack until it misses, where then it should check the other direction until the ship is destroyed. For now, the CPU only attacks randomly. This will be implemented at a later step.
---
+++ battleship_single_player.py
@@ -0,0 +1,241 @@
+import pygame, sys
+from pygame.locals import *
+import numpy as np
+import tkinter as tk
+import random
+
+class Board():
+	def __init__(self,user):
+		self.user = user
+		self.size = 10
+		self.row_list = ("1","2","3","4","5","6","7","8","9","10")
+		self.col_list = ("A","B","C","D","E","F","G","H","I","J")
+		self.cellSize = 55
+		self.borderSize = 5	
+		if (user == "Player"):
+			self.start_xpos = 55
+			self.start_ypos = 55
+		elif (user == "Enemy"):
+			self.start_xpos = 670
+			self.start_ypos = 55
+
+	def draw_axis_titles(self, window, start_xpos, start_ypos, row_pos, col_pos, color):
+		font = pygame.font.SysFont(None, 25)
+		for i in self.row_list:
+			text = font.render(i, 1, color)
+			window.blit(text, (row_pos - (text.get_width()/2), start_ypos))
+			row_pos += self.cellSize
+		for j in self.col_list:
+			text = font.render(j, 1, color)
+			window.blit(text, (start_xpos, col_pos - (text.get_width()/2)))
+			col_pos += self.cellSize
+	
+	# Draws the board starting from the top left corner
+	def draw_board(self, window, color, text_color):
+		start_xpos = self.start_xpos
+		start_ypos = self.start_ypos
+		line_xpos = self.start_xpos
+		line_ypos = self.start_ypos
+		end_xpos = start_xpos + (self.cellSize * self.size)
+		end_ypos = start_ypos + (self.cellSize * self.size)
+		
+		# drawing the borders
+		pygame.draw.line(window, color, (start_xpos, start_ypos), (start_xpos, end_ypos), self.borderSize)
+		pygame.draw.line(window, color, (start_xpos, start_ypos), (end_xpos, start_ypos), self.borderSize)
+		pygame.draw.line(window, color, (start_xpos, end_ypos), (end_xpos, end_ypos), self.borderSize)
+		pygame.draw.line(window, color, (end_xpos, start_ypos), (end_xpos, end_ypos), self.borderSize)
+		
+		# drawing the grid
+		for i in range(self.size):
+			pygame.draw.line(window, color, (line_xpos, start_ypos), (line_xpos, end_ypos))
+			pygame.draw.line(window, color, (start_xpos, line_ypos), (end_xpos, line_ypos))
+			line_xpos += self.cellSize
+			line_ypos += self.cellSize
+	
+		self.draw_axis_titles(window, start_xpos - 25, start_ypos - 25, start_xpos + 15, start_ypos + 15, BLACK)
+		font = pygame.font.SysFont(None, 50)
+		str = self.user + "'s Board"
+		text = font.render(str, 5, text_color)
+		window.blit(text, ((((end_xpos - ((end_xpos - start_xpos)/4)) - text.get_width()), end_ypos + 15)))
+		
+		pygame.display.update()
+
+class Ships():
+	def __init__(self, user, board):
+		self.user = user
+		self.selected = [] # array to keep track of which cells are selected to be ships
+		self.available = ['2', '3', '3', '4', '5']
+		self.board = board
+	
+	def ship_setup(self):
+		size = self.board.size
+		if (self.user == "Player"):
+			root = tk.Tk()
+			root.title('Place your ships!')
+			root.geometry('260x320')
+			frame = tk.Frame(root)
+			frame.grid(row = 0, column = 0)
+			frame['borderwidth'] = 15
+			label = tk.Label(root, text = "Ship Lengths Left: " + ' '.join(str(s) for s in self.available), fg = 'red')
+			
+			self.btn = [[0 for x in range(size)] for x in range(size)]
+			for row in range(size):
+				for col in range(size):
+					index = row * size + col
+					self.btn[row][col] = tk.Button(frame, text = index,  command = lambda x=row, y=col, i = index: self.set_ship(x,y,i))
+					self.btn[row][col].grid(row = row, column = col, sticky = "ew")
+			label.grid()
+			root.mainloop()
+		elif (self.user == "Enemy"):
+			self.cpu_ships(size)
+		
+	def set_ship(self,row,col,index):
+		if (self.btn[row][col].cget('bg') == 'SystemButtonFace'):
+			self.btn[row][col].config(bg ='#008000')
+			self.selected.append(index)
+		elif (self.btn[row][col].cget('bg') == '#008000'):
+			self.btn[row][col].config(bg ='SystemButtonFace')
+			self.selected.remove(index)
+			
+	def draw_ships(self, window, color):
+		start_xpos = self.board.start_xpos
+		start_ypos = self.board.start_ypos
+		cellSize = self.board.cellSize
+		size = self.board.size
+		selected = np.sort(self.selected)
+		for i in selected:
+			pygame.draw.rect(window, color, (start_xpos + cellSize * (i % size), start_ypos + cellSize * (i // size),cellSize, cellSize), 0)
+		pygame.display.update()
+
+	def cpu_ships(self, size):
+		while self.available:
+			for x in self.available:
+				overlap = False
+				orientation = random.randint(0,1) # 0 for horizontal, 1 for vertical
+				if (orientation == 0):
+					row = random.randint(0,(size-1))
+					col = random.randint(0,(size-1)-int(x))
+					index = row * size + col
+					for i in range(int(x)):
+						if (index + i) in self.selected: # check if ship overlaps
+							overlap = True
+							break
+					if (overlap):
+						continue
+					for i in range(int(x)):
+						self.selected.append(index + i)
+				else:
+					row = random.randint(0,(size-1)-int(x))
+					col = random.randint(0,(size-1))
+					index = row * size + col
+					for i in range(int(x)):
+						if (index + i*size) in self.selected:
+							overlap = True
+							break
+					if (overlap):
+						continue	
+					for i in range(int(x)):
+						self.selected.append(index + i*size)
+				self.available.remove(x)
+
+class Missiles():
+	def __init__(self, user, board, ships, enemy_board, enemy_ships):
+		self.user = user
+		self.board = board
+		self.ships = ships
+		self.enemyBoard = enemy_board
+		self.enemyShips = enemy_ships
+		if (user == "Enemy"):
+			self.notattacked = list(range(0,enemy_board.size * enemy_board.size - 1)) # array to keep track of what cells are not hit yet
+	
+	def attack_ui(self, window, enemyMissiles, turn):
+		if (self.user == "Player"):
+			size = self.enemyBoard.size
+			root = tk.Tk()
+			root.title('ATTACK!!!!!')
+			root.geometry('260x320')
+			frame = tk.Frame(root)
+			frame.grid(row = 0, column = 0)
+			frame['borderwidth'] = 15
+			
+			self.btn = [[0 for x in range(size)] for x in range(size)]
+			for row in range(size):
+				for col in range(size):
+					index = row * size + col
+					self.btn[row][col] = tk.Button(frame, text = index,  command = lambda x=row, y=col, i = index, win=window, eBoard=self.enemyBoard, eShips=self.enemyShips, eMissiles=enemyMissiles, turn1=turn: self.fire_missile(x,y,i, win, eBoard, eShips, eMissiles, turn))
+					self.btn[row][col].grid(row = row, column = col, sticky = "ew")
+			root.mainloop()
+		elif (self.user == "Enemy"):
+			pass	
+	
+	def fire_missile(self, row, col, index, window, enemyBoard, enemyShips, enemyMissiles, turn):
+		start_xpos = enemyBoard.start_xpos
+		start_ypos = enemyBoard.start_ypos
+		cellSize = enemyBoard.cellSize
+		size = enemyBoard.size
+		if (self.user == "Player" and turn == 0): # Player attack
+			if (index in enemyShips.selected):
+				pygame.draw.circle(window, (255,0,0), ((start_xpos + cellSize * (index % size)) + cellSize//2, (start_ypos + cellSize * (index // size)) + cellSize//2), cellSize//2, 0)
+				enemyShips.selected.remove(index)
+				self.btn[row][col].config(bg ='#FF0000', state = tk.DISABLED)
+			else:
+				pygame.draw.circle(window, (0,0,0), ((start_xpos + cellSize * (index % size)) + cellSize//2, (start_ypos + cellSize * (index // size)) + cellSize//2), cellSize//2, 0)
+				self.btn[row][col].config(bg='#000000', state = tk.DISABLED)
+				enemyMissiles.fire_missile(row, col, index, window, self.board, self.ships, self, 1)
+		elif (self.user == "Enemy" and turn == 1): # CPU attack
+			index = random.choice(self.notattacked)
+			if (index in enemyShips.selected):
+				pygame.draw.circle(window, (255,0,0), ((start_xpos + cellSize * (index % size)) + cellSize//2, (start_ypos + cellSize * (index // size)) + cellSize//2), cellSize//2, 0)
+				enemyShips.selected.remove(index)
+				self.fire_missile(row, col, index, window, enemyBoard, enemyShips, self, turn)
+			else:
+				pygame.draw.circle(window, (0,0,0), ((start_xpos + cellSize * (index % size)) + cellSize//2, (start_ypos + cellSize * (index // size)) + cellSize//2), cellSize//2, 0)
+			self.notattacked.remove(index)
+		
+		pygame.display.update()
+	
+
+if __name__ == "__main__":
+	# Initialize program
+	pygame.init()
+	pygame.font.init()
+
+	# Game Settings
+	WHITE = (255,255,255)
+	BLACK = (0,0,0)
+	RED = (255,0,0)
+	GREEN = (0,255,0)
+	BLUE = (0,0,255)
+	GREY = (128,128,128)
+
+	# Setting up Display Window
+	SCREEN_WIDTH = 1280
+	SCREEN_HEIGHT = 720
+
+	DISPLAYSURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	DISPLAYSURFACE.fill(WHITE)
+	pygame.display.set_caption("Battleship")
+	
+	turn = random.randint(0,1)
+	playerBoard = Board("Player")
+	enemyBoard = Board("Enemy")
+	playerShip = Ships("Player", playerBoard)
+	playerShip.ship_setup()
+	enemyShip = Ships("Enemy", enemyBoard)
+	enemyShip.ship_setup()
+	playerMissiles = Missiles("Player", playerBoard, playerShip, enemyBoard, enemyShip)
+	enemyMissiles = Missiles("Enemy", enemyBoard, enemyShip, playerBoard, playerShip)
+	
+	#=============
+	# GAME START
+	#=============
+	playerBoard.draw_board(DISPLAYSURFACE, BLACK, BLUE)
+	enemyBoard.draw_board(DISPLAYSURFACE, BLACK, BLUE)
+	playerShip.draw_ships(DISPLAYSURFACE, GREY)
+	enemyShip.draw_ships(DISPLAYSURFACE, GREY)
+	if (turn == 1):
+		enemyMissiles.fire_missile(0, 0, 0, DISPLAYSURFACE, playerBoard, playerShip, playerMissiles, turn)
+		turn = 0
+	playerMissiles.attack_ui(DISPLAYSURFACE, enemyMissiles, turn)
+	
+	pygame.quit()
